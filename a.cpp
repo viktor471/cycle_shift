e8ebdce281cca4a4da7af7f97a325d523ff8eeda
viktor borodin <vikbrdin@gmail.com>
2021-02-28 15:10:45 +0000

    add setw and rvalue references in shift function

--- a.cpp
+++ a.cpp
@@ -1,32 +1,10 @@
 #include <iostream>
 #include <vector>
 #include <ctime>
+#include <iomanip>
 
-//template< typename T>
-//void shift( T& begin, T& end )  
-//{
-//	if( begin == end )
-//		return;
-//
-//	auto temp = *begin;
-//	if( end - 1 != begin )
-//	{
-//		temp = *(end - 1);	
-//	}
-//
-//	for( T it = end - 1; it != begin; it-- )
-//	{
-//		*(it - 1) = *it;	
-//	}
-//
-//   *begin = temp;
-//}
-//
-
-//template < class Iterator, 
-//	  		  class U = typename std::iterator_traits<Iterator>::value_type >
 template < class Iterator >
-void shift( Iterator begin, Iterator end )  
+void shift( Iterator&& begin, Iterator&& end )  
 {
 	if( begin == end )
 		return;
@@ -61,7 +39,7 @@ void show_vector( const std::vector<T> &values )
 
 	for( const int &val : values )
 	{
-		std::cout << val << " ";
+		std::cout << std::setw(2) << val << " ";
 	}
 
 	std::cout << std::endl;
