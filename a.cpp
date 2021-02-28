ba1fcee0b3b19bdd11db91ee2f6c421a81ceb29b
viktor borodin <vikbrdin@gmail.com>
2021-02-28 17:50:27 +0000

    not working

--- a.cpp
+++ a.cpp
@@ -7,7 +7,7 @@ enum Direction
 {
 	LEFT,
 	RIGHT
-}
+};
 
 template < class Iterator >
 void shift( const Iterator& begin, 
@@ -15,22 +15,40 @@ void shift( const Iterator& begin,
 				Direction dir = LEFT )  
 {
 
-	if(    ( ( begin    ) == end ) 
-		 || ( ( begin -1 ) == end ) )
-	{
+	if(  ( begin    ) == end  )
 		return;
-	}
+   
+	if(  ( begin -1 ) == end  )
+		return;
+
+	Iterator temp;	
 
 	if( dir == LEFT )
 	{
-		temp = *begin;	
+
+		*temp = *begin;	
 	
 		for( Iterator it = begin + 1; it != end; it++ )
 		{
-			*(it - 1) = *it;	
+			*( it - 1 ) = *it;	
 		}
 
-   	*(end - 1) = temp;
+   	*(end - 1) = *temp;
+
+	}
+
+
+	if( dir == RIGHT )
+	{
+
+		*temp = *end;
+
+		for( Iterator it = end - 1; it != begin; it-- )
+		{
+			*it = *( it - 1 );
+		}
+		*begin = *temp;
+
 	}
 
 }
@@ -38,6 +56,7 @@ void shift( const Iterator& begin,
 
 void fill_array( std::vector<int>& values )
 {
+	srand( time(NULL) );
 	for( int &val : values )
 	{
 		val = rand()%100;
@@ -60,21 +79,33 @@ void show_vector( const std::vector<T> &values )
 
 int main()
 {
-	srand( time(NULL) );
-
 	const size_t len = 10;
 
 	std::vector<int> values( len );
 
 	fill_array( values );
 
+/////////----LEFT_SHIFT----////////////////////////////
+
+	std::cout << "before LEFT shift: " << std::endl;
 	show_vector( values );
 
-	shift( values.begin(), values.end() );
+	shift( values.begin(), values.end(), LEFT );
 
 
+	std::cout << "after LEFT shift: " << std::endl;
 	show_vector( values );
 
+/////////----RIGHT_SHIFT----//////////////////////////
+
+	std::cout << "before RIGHT shift: " << std::endl;
+	show_vector( values );
+	
+	shift( values.begin(), values.end(), RIGHT );
+	
+	std::cout << "after RIGHT shift: " << std::endl;
+	show_vector( values );
+	
 	return 0;
 }
 
